c4d0d95ba2512048b32c9f26dbc2d06df04ebfb3
Robert McBride <robmcbride@gmail.com>
2018-02-12 22:23:42 -0800

    basic plot generation of a symbol

--- symbols.go
+++ symbols.go
@@ -5,6 +5,10 @@ import (
 	"fmt"
 	"log"
 	"time"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotutil"
+	"gonum.org/v1/plot/vg"
 )
 
 type Candle struct {
@@ -21,8 +25,34 @@ func (c Candle) String() string {
 	return prettyJSON(c)
 }
 
-type candlestickReq struct {
+type Candles struct {
 	Candles []Candle `json:"candles"`
+}
+
+func (c Candles) Len() int {
+	return len(c.Candles)
+}
+
+func (list Candles) XY(i int) (float64, float64) {
+	c := list.Candles[i]
+	X, err := c.dayFloat()
+	if err != nil {
+		panic(err)
+	}
+	Y := c.Close
+	return X, Y
+}
+
+func (c Candle) dayFloat() (float64, error) {
+	d, err := time.Parse(time.RFC3339Nano, c.Start)
+	if err != nil {
+		return 0, fmt.Errorf("error parsing date for candle %s, %v", c.Start, err)
+	}
+	// dStr := fmt.Sprintf("%d%02d%02d", d.Year(), d.Month(), d.Day())
+	// fmt.Println(dStr)
+	// dFlt, _ := strconv.ParseFloat(dStr, 64)
+	// return dFlt, nil
+	return float64(d.Unix()), nil
 }
 
 func requestCandles(symbolID string) error {
@@ -38,17 +68,43 @@ func requestCandles(symbolID string) error {
 		return fmt.Errorf("error requesting candles, %v", err)
 	}
 
-	data := &candlestickReq{}
+	data := &Candles{}
 	err = json.Unmarshal(res, data)
 	if err != nil {
 		return fmt.Errorf("error parsing JSON: %s", err)
 	}
 
-	for i, candle := range data.Candles {
-		log.Printf("Candle %s: %s", string(i), candle)
-	}
+	// for i, candle := range data.Candles {
+	// log.Printf("Candle %s: %s", string(i), candle)
+	// }
+
+	drawPlot(symbolID, *data)
 
 	// log.Printf("%+v\n", accounts)
+
+	return nil
+}
+
+func drawPlot(symbolID string, candles Candles) error {
+	p, err := plot.New()
+	if err != nil {
+		return fmt.Errorf("error constructing plot, %v", err)
+	}
+
+	p.X.Tick.Marker = plot.TimeTicks{}
+
+	p.Title.Text = "Thing"
+	p.X.Label.Text = "Date"
+	p.Y.Label.Text = "Price"
+
+	err = plotutil.AddLinePoints(p, symbolID, candles)
+	if err != nil {
+		return fmt.Errorf("error plotting points, %v", err)
+	}
+
+	if err := p.Save(10*vg.Inch, 10*vg.Inch, "tmp/plot.svg"); err != nil {
+		return fmt.Errorf("error creating the plot image, %v", err)
+	}
 
 	return nil
 }
